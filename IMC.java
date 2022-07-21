f2240dbbb985740e64e6cafd75d8807ce6344f4d
Bruno Boeno Delgado <92734464+brunobdelgado@users.noreply.github.com>
2022-07-21 10:18:58 -0300

    Add files via upload

--- IMC.java
+++ IMC.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package aula.facul;
 
-/**
- *
- * @author bruno.delgado
- */
 import java.util.Scanner;
 
-public class AulaFacul {
+public class IMC {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
